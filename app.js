0a54fa5c7caea5bfeac95e85c924f12cf12a7456
Denis <denisstepanenko6@gmail.com>
2024-02-28 16:52:53 +0200

    Add sequelize dbErrorHandler

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = require('./routes');
-const { errorHendler } = require('./middleware/errorHandlers');
+const { errorHendler, dbErrorHendler} = require('./middleware/errorHandlers');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', router);
+app.use(dbErrorHendler);
 app.use(errorHendler);
 module.exports = app;
--- middleware/errorHandlers.js
+++ middleware/errorHandlers.js
@@ -1,3 +1,20 @@
+const createHttpError = require('http-errors');
+const { ValidationError, BaseError } = require('sequelize');
+
+module.exports.dbErrorHendler = (err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    const error = err.errors.map(element => ({
+      message: element.message,
+      status: 422,
+    }));
+    return res.status(error[0].status).send(error[0].message);
+  }
+  if (err instanceof BaseError)
+    return next(createHttpError(500, 'Database Error'));
+
+  next(err);
+};
+
 module.exports.errorHendler = (err, req, res, next) => {
   if (res.HeaderSent) return;
 
