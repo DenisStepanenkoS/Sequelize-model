6cd14e86006c51c7aa3b7d43ee642a20ca6a96d5
Denis <denisstepanenko6@gmail.com>
2024-03-02 20:28:17 +0200

    Fix phone belogsToMany

--- models/phone.js
+++ models/phone.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       Phone.belongsToMany(models.Customer, {
-        through: 'orders',
+        through: 'Order',
         foreignKey: 'phoneId',
       });
     }
